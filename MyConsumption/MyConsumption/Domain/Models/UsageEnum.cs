6964e5a5c118902d26a1a3f6be1c688310f7c9df
DavinaLauwers <lauwers.davina@icloud.com>
2024-03-18 13:11:58 +0100

    Add UsageEnum

---
+++ MyConsumption/MyConsumption/Domain/Models/UsageEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyConsumption.Domain.Models
+{
+    public enum UsageEnum
+    {
+        Elektriciteit,
+        Water
+
+    }
+}
