eba5e7bd59ec8c20a8b1e52025168e784b780a3d
DavinaLauwers <lauwers.davina@icloud.com>
2024-03-18 13:21:58 +0100

    Add BaseEntity

---
+++ MyConsumption/MyConsumption/Domain/Models/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyConsumption.Domain.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public UsageEnum Usage { get; set; }
+        public float UsageAmount { get; set; }
+    }
+}
